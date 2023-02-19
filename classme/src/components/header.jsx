7ab7755d66a7fe45fc33232ed9c2349d721fd0ef
Bruno Wotzke <brunoricardowotzke@gmail.com>
2023-02-18 23:14:48 -0300

    .

---
+++ classme/src/components/header.jsx
@@ -0,0 +1,13 @@
+export default function Header(){
+    return(
+        <header>
+            <div>
+                <div>Logo</div>
+            </div>
+            <div>
+                <a href="#">Login</a>
+                <a href="#">Cadastro</a>
+            </div>
+        </header>
+    )
+}
